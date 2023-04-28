fc50e63c251ab551bed8cc5056ee3c1876e55031
一个啵儿 <82209188+15765574241@users.noreply.github.com>
2023-04-28 11:32:28 +0800

    test

--- src/api/ad.js
+++ src/api/ad.js
@@ -1,10 +1,11 @@
-import request from '@/utils/request'
+import request from "@/utils/request";
 
 export function getList(data) {
+  console.log(9999);
   return request({
     //url: '/ad/getList',
-    url: 'https://851edf02-46eb-43e6-828d-64c7e483ea41.bspapp.com/http/getAd',
-    method: 'get',
+    url: "https://851edf02-46eb-43e6-828d-64c7e483ea41.bspapp.com/http/getAd",
+    method: "get",
     data,
-  })
+  });
 }
